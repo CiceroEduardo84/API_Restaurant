fd35731bef395778454c9b63b93f89194fdb4ca8
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2025-08-28 22:54:58 -0300

    feat: add orders controler and routes

---
+++ src/controllers/ordersController.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
+class OrdersController {
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bodyScheme = z.object({
+        table_session_id: z.number(),
+        product_id: z.number(),
+        quantity: z.number(),
+      });
+
+      const { table_session_id, product_id, quantity } = bodyScheme.parse(
+        req.body
+      );
+
+      return res.status(201).json({});
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { OrdersController };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { productsRoutes } from "./productsRoutes.js";
 import { tablesRoutes } from "./tableRoutes.js";
 import { tablesSessionsRouter } from "./tablesSessionsRoutes.js";
+import { ordersRoutes } from "./ordersRoutes.js";
 
 const routes = Router();
 
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRoutes);
 routes.use("/tables_sessions", tablesSessionsRouter);
+routes.use("/orders", ordersRoutes);
 
 export { routes };
